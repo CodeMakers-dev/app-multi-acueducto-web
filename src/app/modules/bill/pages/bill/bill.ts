b7878f53483a88ba33e7fa4299c1c44f52428d2f
celuladevarquitectura <celuladev.arquitectura@col.bellota.com>
2025-07-16 14:26:16 -0500

    file server instalacion

--- src/app/modules/bill/pages/bill/bill.ts
+++ src/app/modules/bill/pages/bill/bill.ts
@@ -10,6 +10,7 @@ import { FacturaService } from '../../service/factura.service';
 import { ApiResponse } from '@interfaces/Iresponse';
 import { ToastService } from '@services/toast.service';
 import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 
 @Component({
