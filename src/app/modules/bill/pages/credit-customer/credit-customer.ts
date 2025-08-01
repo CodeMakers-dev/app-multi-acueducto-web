5dac7d55493519e4b80cab144db48c60795b144b
DESKTOP-DL3TM1L\ADMIN <nicope2004@gmail.com>
2025-08-01 11:38:20 -0500

    Ajustes modulo Abono Factura y ajuste de servicio de consulta

--- src/app/modules/bill/pages/credit-customer/credit-customer.ts
+++ src/app/modules/bill/pages/credit-customer/credit-customer.ts
@@ -1,97 +1,113 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Table } from '@components/table/table';
-import { TableColumn } from '@interfaces/ItableColumn';
 import { AbonoService } from '../../service/abono.service';
 import { ApiResponse } from '@interfaces/Iresponse';
 import { IAbonoFactura } from '@interfaces/IdeudaFactura';
+import { TableComponent } from '@components/table';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-credit-customer',
-  imports: [ CommonModule, Table, RouterModule],
-  templateUrl: './credit-customer.html',
+  imports: [ CommonModule, TableComponent, RouterModule],
+  template: `
+  
+    <app-table-dynamic
+        [title]="title()"
+        [datasource]="creditCustomerData()"
+        [columns]="creditCustomerColumns()"
+      />
+  `,
 
 })
-export class CreditCustomer implements OnInit {
+export class CreditCustomer {
 
-  abonoColumns: TableColumn[] = [
-    { key: 'clienteFactura', label: 'Cliente', sortable: true },
-    { key: 'codigoFactura', label: 'Código factura', sortable: true },
-    { key: 'fechaAbonoTexto', label: 'Fecha abono', sortable: true },
-    { key: 'valorTexto', label: 'Valor abono', sortable: true }
-  ];
+  creditCustomerColumns = signal([
+    { field: 'nombreCliente', header: 'Cliente' },
+    { field: 'codigoFactura', header: 'Código Factura' },
+    { field: 'fechaAbono', header: 'Fecha Abono' },
+    { field: 'valorAbono', header: 'Valor Abono' },
+  ]);
+  creditCustomerData = computed(() => this.dataCreditCustomer.value() ?? []);
+  title = signal('Abono Facturas');
+
   tableData: any[] = [];
-  totalRegisters: number = 0;
-
-  currentPage: number = 1;
-  pageSize: number = 10;
-
-  searchTerm: string = '';
-  currentSortColumn: string = '';
-  currentSortDirection: 'asc' | 'desc' = 'asc';
 
   protected readonly abonoService = inject(AbonoService);
 
-  ngOnInit(): void {
-    this.loadAbono();
+
+  constructor() {
+    effect(() => {
+      console.log(
+        'Data loaded______:',
+        this.dataCreditCustomer.value()
+      );
+    });
   }
 
-  loadAbono(): void {
-    this.abonoService.getAllAbono(
-      this.currentPage,
-      this.pageSize,
-      this.searchTerm,
-      this.currentSortColumn,
-      this.currentSortDirection
-    ).subscribe(
-      (apiResponse: ApiResponse<IAbonoFactura[]>) => {
-        const abonos = apiResponse.response;
+  dataCreditCustomer = rxResource({
+  stream: () =>
+    this.abonoService.getAllAbono().pipe(
+      map((apiRes: ApiResponse<IAbonoFactura[]>) =>
+        apiRes.response.map(abono => {
+          const cliente = abono.deudaCliente.empresaClienteContador.cliente;
 
-        const abonosTransformados = abonos.map(abono => {
-          const valorDeuda = abono.deudaCliente?.valor || '0';
-          const valorAbono = abono.valor || '0';
-          const codigoFactura = abono.deudaCliente?.factura?.codigo || 'Sin código';
+          const fullName = [
+            cliente.nombre || '',
+            cliente.segundoNombre || '',
+            cliente.apellido || '',
+            cliente.segundoApellido || ''
+          ].filter(Boolean).join(' ');
 
-          const cliente = abono.deudaCliente?.empresaClienteContador?.cliente;
-
-          const clienteNombreCompleto = cliente
-            ? `${cliente.nombre ?? ''} ${cliente.segundoNombre ?? ''} ${cliente.apellido ?? ''} ${cliente.segundoApellido ?? ''}`.trim().replace(/\s+/g, ' ')
-            : 'Sin cliente';
+          const fechaAbono = new Date(abono.fechaCreacion);
 
           return {
-            ...abono,
-            codigoFactura,
-            clienteFactura: clienteNombreCompleto,
-            valorTexto: `$${parseFloat(valorAbono).toLocaleString('es-CO')}`,
-            fechaAbonoTexto: abono.fechaCreacion?.toString().slice(0, 10)
+            id: abono.id,
+            nombreCliente: fullName,
+            codigoFactura: abono.deudaCliente.factura || 'Sin código',
+            fechaAbono: fechaAbono.toLocaleDateString('es-CO'),
+            valorAbono: `$${Number(abono.valor).toLocaleString()}`
           };
-        });
-
-        this.totalRegisters = abonos.length;
-        this.tableData = abonosTransformados;
-      },
-      error => {
-        console.error('Error al cargar los abonos:', error);
-      }
-    );
-  }
+        })
+      )
+    )
+});
 
 
-  onPageChange(newPage: number): void {
-    this.currentPage = newPage;
-    this.loadAbono();
-  }
 
-  onSortChange(event: { column: string; direction: 'asc' | 'desc' }): void {
-    this.currentSortColumn = event.column;
-    this.currentSortDirection = event.direction;
-    this.loadAbono();
-  }
+  // loadAbono(): void {
+  //   this.abonoService.getAllAbono(
+  //   ).subscribe(
+  //     (apiResponse: ApiResponse<IAbonoFactura[]>) => {
+  //       const abonos = apiResponse.response;
 
-  onSearchInput(event: Event): void {
-    this.searchTerm = (event.target as HTMLInputElement).value;
-    this.currentPage = 1;
-    this.loadAbono();
-  }
+  //       const abonosTransformados = abonos.map(abono => {
+  //         const valorDeuda = abono.deudaCliente?.valor || '0';
+  //         const valorAbono = abono.valor || '0';
+  //         const codigoFactura = abono.deudaCliente?.factura?.codigo || 'Sin código';
+
+  //         const cliente = abono.deudaCliente?.empresaClienteContador?.cliente;
+
+  //         const clienteNombreCompleto = cliente
+  //           ? `${cliente.nombre ?? ''} ${cliente.segundoNombre ?? ''} ${cliente.apellido ?? ''} ${cliente.segundoApellido ?? ''}`.trim().replace(/\s+/g, ' ')
+  //           : 'Sin cliente';
+
+  //         return {
+  //           ...abono,
+  //           codigoFactura,
+  //           clienteFactura: clienteNombreCompleto,
+  //           valorTexto: `$${parseFloat(valorAbono).toLocaleString('es-CO')}`,
+  //           fechaAbonoTexto: abono.fechaCreacion?.toString().slice(0, 10)
+  //         };
+  //       });
+
+  //       this.tableData = abonosTransformados;
+  //     },
+  //     error => {
+  //       console.error('Error al cargar los abonos:', error);
+  //     }
+  //   );
+  // }
+
 }
